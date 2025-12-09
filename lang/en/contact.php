0795d45916604d3ebb0ffb20c88fc2f5fe61f14e
hrnicek <hrncir39@gmail.com>
2025-12-09 23:54:53 +0100

    wip

---
+++ lang/en/contact.php
@@ -0,0 +1,53 @@
+<?php
+
+return [
+    'page_header' => [
+        'title' => 'Contact',
+        'subtitle' => 'Get in touch',
+    ],
+    'ski_areal' => [
+        'title' => 'SKI area Kunčice',
+        'company' => 'SKITECH s.r.o.',
+        'address' => 'Kunčice 56, Staré Město pod Sněžníkem, 78832',
+        'gps' => 'GPS: 50°11\'16.044"N, 16°56\'21.372"E',
+        'tax_info' => 'Company ID: 26827182, VAT ID: CZ 26827182',
+    ],
+    'phone_contacts' => [
+        'title' => 'Phone contacts',
+        'c1' => 'Ski courses',
+        'c2' => 'Reception 8:00-16:00',
+        'c3' => 'Ski school',
+        'c4' => 'Accommodation',
+        'c5' => 'Pub',
+    ],
+    'specialized_services' => [
+        'title' => 'Specialized services',
+        's1' => [
+            'title' => 'Ski school and rental',
+            'company' => 'NEWMAN SCHOOL',
+        ],
+        's2' => [
+            'title' => 'Accommodation',
+            'subtitle' => 'Accommodation booking',
+            'link' => 'Accommodation overview >>',
+        ],
+    ],
+    'operating_info' => [
+        'title' => 'Operating information',
+        'i1' => [
+            'title' => 'Opening hours',
+            'p1' => 'Depending on snow conditions',
+            'p2' => 'Reception: 8:00 - 16:00',
+        ],
+        'i2' => [
+            'title' => 'Parking',
+            'p1' => 'Free at the area',
+            'p2' => 'Sufficient capacity',
+        ],
+        'i3' => [
+            'title' => 'Restaurant',
+            'p1' => 'Hot refreshments',
+            'p2' => 'Tel: +420 739 524 093',
+        ],
+    ],
+];
