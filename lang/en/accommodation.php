e19a6c4e5cc1287d1f1889a40bd97155b7da79c4
hrnicek <hrncir39@gmail.com>
2025-12-10 08:24:31 +0100

    wip

---
+++ lang/en/accommodation.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'index' => [
+        'page_header' => [
+            'title' => 'Accommodation',
+            'subtitle' => 'We cooperate with the owners of guest houses and cottages in Kunčice, so we can find the most suitable accommodation for you. Whether there are just two of you, or a whole expedition!',
+        ],
+        'content' => 'Accommodation options.',
+    ],
+    'show' => [
+        'page_header' => [
+            'title' => 'Accommodation details',
+            'subtitle' => 'Details about accommodation',
+        ],
+        'content' => 'Details about accommodation.',
+    ],
+];
